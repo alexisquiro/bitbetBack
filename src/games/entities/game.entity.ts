5072015864b820caf36bb51056cdaf1b267c1400
alexis <alexis.work.quiros@gmail.com>
2024-01-25 12:30:57 -0400

    proyecto

---
+++ src/games/entities/game.entity.ts
@@ -0,0 +1,7 @@
+export class Game {
+    team1: string;
+    team2: string;
+    dateCreation: string;
+    dateGame:string;
+    winner:number;
+}
---
+++ src/users/entities/user.entity.ts
@@ -0,0 +1,5 @@
+export class User {
+    logged: boolean;
+    name: string;
+    email: string;
+}
